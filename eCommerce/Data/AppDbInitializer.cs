8a1cc10ccb7b7c77b8329a1e0a9b826706ffbb47
Acer <Acer@DESKTOP-7KRI1FA>
2023-03-18 20:41:43 +0545

    Actors View Added

--- eCommerce/Data/AppDbInitializer.cs
+++ eCommerce/Data/AppDbInitializer.cs
@@ -10,10 +10,34 @@
                 context.Database.EnsureCreated();
 
                 //Cinema
+                if (!context.Cinemas.Any())
+                {
+
+                }
+
                 //Actors
+                if (!context.Actors.Any())
+                {
+
+                }
+
                 //Producers
+                if (!context.Producers.Any())
+                {
+
+                }
+
                 //Movies
+                if (!context.Movies.Any())
+                {
+
+                }
+
                 //Actors and Movies
+                if (!context.Actors_Movies.Any())
+                {
+
+                }
             }
         }
     }
